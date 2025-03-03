5ef85e5eb3de490394a380279cfc7f389d2d50bf
DeveloperIlhombek <ilxomdeveloper@gmail.com>
2025-03-03 14:16:21 +0500

    keraksiz kodlar o'chirildi

--- app/[locale]/test/page.tsx
+++ app/[locale]/test/page.tsx
@@ -1,18 +1,12 @@
 import { getAllTests } from '@/lib/api'
 import Test from './_components/test'
 
-// interface PageParams {
-// 	lang: string
-// }
-
 export default async function TestPage({
 	params,
 }: {
 	params: Promise<{ locale: string }>
 }) {
 	const { locale } = await params
-	console.log('Test' + '   ' + locale)
-
 	const data = await getAllTests({
 		pageSize: 720,
 		pageNumber: 2,
